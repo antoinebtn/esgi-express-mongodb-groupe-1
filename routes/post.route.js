e6317f8525ef09031f58d1ae23b176c5eb1b8e39
antoinebtn <antoinebtn@gmail.com>
2025-04-04 16:01:47 +0200

    Auth Middleware on posts route

--- routes/post.route.js
+++ routes/post.route.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const AuthMiddleware = require("./../middleware/auth.middleware")
 
 const postController = require("../controller/post.controller");
 
 router.get("/", postController.getAll);
-router.post("/", postController.create);
-router.put("/:id", postController.update);
-router.delete("/:id", postController.delete);
+router.post("/", AuthMiddleware, postController.create);
+router.put("/:id", AuthMiddleware, postController.update);
+router.delete("/:id", AuthMiddleware, postController.delete);
 
 module.exports = router;
