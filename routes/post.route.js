3993f36955f8e3e19a1dc5324cc853dad85dbc0c
Yannis Alouache <alouacehyannis1@gmail.com>
2025-04-04 15:11:46 +0200

    feat: add path

--- routes/post.route.js
+++ routes/post.route.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const postController = require("../controllers/post.controller");
+const postController = require("../controller/post.controller");
 
+router.post("/", postController.create);
 router.put("/:id", postController.update);
