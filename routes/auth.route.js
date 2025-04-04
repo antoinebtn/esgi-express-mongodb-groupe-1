d9c0e39547fc45a6504c3261211c785f32846f81
antoinebtn <antoinebtn@gmail.com>
2025-04-04 15:15:33 +0200

    signin + Login

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,4 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const userController = require('../controller/user.controller.js');
 
 
 router.post('/signin', userController.signin);
 router.post('/login', userController.login);
+
+module.exports = router;
