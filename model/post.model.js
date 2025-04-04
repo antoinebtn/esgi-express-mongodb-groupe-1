83fdc0f89e4fda5165ef147c9bce3ea5651f4332
Yannis Alouache <alouacehyannis1@gmail.com>
2025-04-04 14:59:52 +0200

    feat: add author field to Post schema

--- model/post.model.js
+++ model/post.model.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const post = new Schema({
     text: { type: String, required: true },
     image: { type: String, required: false },
+    author: { type: String, required: true },
 });
 
 const Post = mongoose.model('Post',post);
