779ff7eb2221d9ef4c13ecfc508d46c225859f1c
louis <louisguidez3@gmail.com>
2025-04-04 15:17:58 +0200

    Ajout de routes delete et getAll  pour les posts

--- routes/post.route.js
+++ routes/post.route.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const postController = require("../controllers/post.controller");
+const postController = require("../controller/post.controller");
 
+router.get("/", postController.getAll);
 router.put("/:id", postController.update);
+router.delete("/:id", postController.delete);
+
